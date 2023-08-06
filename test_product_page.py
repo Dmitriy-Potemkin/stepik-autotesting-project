f40837d98bd2f65c6b98f576fe05bef7be135234
Dmitriy Potemkin <dpotemkin@ptsecurity.com>
2023-08-06 15:36:42 +0300

    added user registration

--- test_product_page.py
+++ test_product_page.py
@@ -2,7 +2,33 @@ from pages.login_page import LoginPage
 from pages.product_page import ProductPage
 from pages.basket_page import BasketPage
 import pytest
+import time
 
+
+class TestUserAddToBasketFromProductPage():
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        login_page = LoginPage(browser, "http://selenium1py.pythonanywhere.com/accounts/login/")
+        login_page.open()
+        email, password = f"{str(time.time())}@fakemail.com", str(time.time())
+        login_page.register_new_user(email, password)
+        login_page.should_be_authorized_user()
+    
+    def test_user_cant_see_success_message(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_not_be_success_message()
+    
+    def test_user_can_add_product_to_basket(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
+        page = ProductPage(browser, link)
+        page.open()
+        item_name = page.get_product_name()
+        item_value = page.get_product_value()
+        page.add_to_basket()
+        page.should_be_correct_bought_item(item_name)
+        page.should_be_correct_bought_value(item_value)
 
 def test_guest_should_see_add_to_basket_button(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
